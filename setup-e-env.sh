6d574fb151d5f97a972eae0b8a433a24079359a5
Etuloser <1494136313@qq.com>
2019-10-10 10:20:44 +0800

    add script to init my work lab

---
+++ setup-e-env.sh
@@ -0,0 +1,9 @@
+mkdir -p /usr/local/src/develop
+ln -s /usr/local/src/develop /develop
+mkdir -p /develop/package
+mkdir -p /develop/script
+mkdir -p /develop/project
+
+yum install -y unzip
+yum install -y lrzsz
+yum install -y vim
