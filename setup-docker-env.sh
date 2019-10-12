232dbde76073018ec19ea490b7521bfc8e5cb9d8
Etuloser <1494136313@qq.com>
2019-10-12 11:25:24 +0800

    merge scripts to setup-docker-env.sh

---
+++ setup-docker-env.sh
@@ -0,0 +1,16 @@
+# apply to centos7
+# include docker-ce and docker-compose environment
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+yum makecache fast
+yum -y install docker-ce
+systemctl start docker
+systemctl enable docker
+curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://f1361db2.m.daocloud.io
+systemctl restart docker
+docker --version
+
+curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose --version
